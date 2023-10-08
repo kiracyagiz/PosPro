ee9415d0dabf4f2e00919f8c7c02e05c84b18ecd
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-10-08 12:25:09 +0200

    proje

--- components/Blog/Blog.jsx
+++ components/Blog/Blog.jsx
@@ -1,30 +1,60 @@
-import React from "react";
 import BlogBox from "../Blog/BlogBox";
-import blog2 from "../../public/blog2.png";
-import blog3 from "../../public/blog3.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ButtonSecondary from "../General/ButtonSecondary";
+import Image from "next/image";
+
 import { useTranslation } from "next-i18next";
 
-
 const Blog = () => {
-    const {t} = useTranslation("common")
-    const blogData = t("common:Blog",{returnObjects: true})
+  const { t } = useTranslation("common");
+  const blogData = t("common:Blog", { returnObjects: true });
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className="bg-gray-100 py-14">
       <p className="text-center text-3xl font-bold ">BLOG</p>
-    
 
-      <div className="flex flex-wrap md:mx-20 justify-center gap-x-32 items-center gap-y-12 mt-14">
-        {blogData.map((dt,i) => (
-            <BlogBox
+      <div className="hidden lg:flex lg:flex-wrap md:mx-20 justify-center gap-x-32 items-center gap-y-12 mt-14">
+        {blogData.map((dt, i) => (
+          <BlogBox
             image={dt.image}
             title={dt.title}
             desc={dt.desc}
             location={dt.location}
             key={i}
             button={dt.button}
-            />
+          />
         ))}
+      </div>
 
+      <div className=" w-4/5 mx-auto p-20 block lg:hidden">
+        <Slider {...settings}>
+          {blogData.map((dt, i) => (
+              <div key={i} className="flex flex-col  border-2  rounded-lg border-black shadow-xl bg-white hover:scale-105 hover:shadow-md cursor-pointer hover:shadow-black  transition-transform duration-300">
+               <div className="w-4/5 mx-auto">
+               <img src={dt.image} className="w-full p-4 mx-auto"  alt={dt.title} />
+               </div>
+                <a href={dt.location}>
+                  <div className="p-4 flex flex-col gap-y-4">
+                    <p className="font-bold text-primaryYellow">
+                      Posted on 28/09/2023
+                    </p>
+                    <h3 className="roboto700 text-sm">{dt.title}</h3>
+                    <p className="max-w-lg text-sm">{dt.desc}</p>
+                    <ButtonSecondary text={dt.button} />
+                  </div>
+                </a>
+              </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
--- components/Blog/BlogBox.jsx
+++ components/Blog/BlogBox.jsx
@@ -3,9 +3,9 @@ import ButtonSecondary from "../General/ButtonSecondary";
 import Link from "next/link";
 const BlogBox = ({ image, title, desc, location ,button}) => {
   return (
-    <div className="w-70 md:w-96 lg:w-70 ">
+    <div className="w-70 md:w-full  lg:w-70 flex flex-col items-center justify-center">
       <div className="flex flex-col border-2  rounded-lg border-black shadow-xl bg-white hover:scale-105 hover:shadow-md cursor-pointer hover:shadow-black  transition-transform duration-300">
-       <Image src={image} width={800} height={133}alt={title} />
+      <img src={image} className="w-full p-4 mx-auto"  alt={title} />
         <a href={location}>
           <div className="p-4 flex flex-col gap-y-4">
             <p className="font-bold text-primaryYellow">Posted on 28/09/2023</p>
