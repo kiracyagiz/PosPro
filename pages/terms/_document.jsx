d893be54c6a6f1e026840832051ba7996528e592
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-10-17 09:46:30 +0200

    updated

---
+++ pages/terms/_document.jsx
@@ -0,0 +1,38 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="description"
+            content="Jemi një kompani teknologjike që specializohet në programe për lokale dhe menaxhimin e porosive, me fokus te inovacioni dhe kënaqësia e klientit."
+          />{" "}
+          <link rel="stylesheet" />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=GTM-WZ44XMQ7"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                    
+                      gtag('config', 'GTM-WZ44XMQ7');
+            `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
