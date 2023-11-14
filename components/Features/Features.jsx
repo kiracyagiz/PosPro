7e861b29b29d636d25f4bfb48376b065a506966c
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-11-14 16:30:00 +0100

    language

--- components/Features/Features.jsx
+++ components/Features/Features.jsx
@@ -13,8 +13,7 @@ const Features = () => {
       <h2 className="text-3xl font-semibold text-center ">
         {titles.features}
       </h2>
-      <div>  
-      </div>
+  
       <div className="flex flex-col items-center  bg-gray-100">
   {featuresData.map((dt, i) => (
     <FeaturesCard dt={dt} key={i} className="w-1/2 md:w-1/3 lg:w-1/4" />
@@ -22,9 +21,6 @@ const Features = () => {
 </div>
     
 
-      <div>
-     
-      </div>
     </div>
   );
 };
