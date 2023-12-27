180259a8b8307f51f4210226a7f35770bfa911e2
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-12-27 16:53:21 +0100

    deneme osn

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,10 +1,19 @@
-"use client"
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import Image from 'next/image';
+import ButtonPrimary from '../components/General/ButtonPrimary';
+const NotFound = () => {
+  const router = useRouter();
 
-import React from 'react'
+  useEffect(() => {
+    // Belirli bir süre sonra, örneğin 3 saniye sonra error sayfasına yönlendir.
+    const redirectTimer = setTimeout(() => {
+      router.push('/');
+    }, 3000);
 
-import Image from 'next/image'
-import ButtonPrimary from '../components/General/ButtonPrimary'
-const NotFoundPage  = () => {
+    return () => clearTimeout(redirectTimer);
+  }, [router]);
+
   return (
     <div className='text-center justify-center items-center flex flex-col gap-y-10 p-20'>
     <Image src={'/notfound.png'} width={200} height={200}></Image>
@@ -12,7 +21,8 @@ const NotFoundPage  = () => {
     <p className='text-xl'>You may have mistyped the address or the page may have moved.</p>
     <ButtonPrimary text={'Go Home'} locate={'/'}/>
     </div>
-  )
-}
 
-export default NotFoundPage 
+  );
+};
+
+export default NotFound;
