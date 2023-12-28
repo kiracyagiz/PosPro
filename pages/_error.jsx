80be9fc366a56716fefc28f337b54385b070efe0
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-12-28 16:34:58 +0100

    build

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -8,7 +8,7 @@ const _error = () => {
     <img src='/notfound.png' alt='Not Found' width='200' height='200'/>
     <p className='text-3xl'>Oops. This page has gone missing.</p>
     <p className='text-xl'>You may have mistyped the address or the page may have moved.</p>
-    <ButtonPrimary text={'Go Home'}/>
+    <ButtonPrimary text={'Go Home'} locate={'/'}/>
   </div>
   )
 }
