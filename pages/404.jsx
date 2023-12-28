623c5652ec27b084aadb69d95788c5b246537a20
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-12-28 09:02:04 +0100

    link fixed

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const notFound = () => {
@@ -6,7 +7,7 @@ const notFound = () => {
     <img src='/notfound.png' alt='Not Found' width='200' height='200'/>
     <p className='text-3xl'>Oops. This page has gone missing.</p>
     <p className='text-xl'>You may have mistyped the address or the page may have moved.</p>
-    <a href='/' className='bg-blue-500 text-white px-4 py-2 rounded'>Go Home</a>
+    <Link href='/' className='bg-blue-500 text-white px-4 py-2 rounded'>Go Home</Link>
   </div>
   )
 }
