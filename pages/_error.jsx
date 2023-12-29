643b6a111c15705398b7e1862f4ff1ab1e6a4dd3
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-12-29 11:29:46 +0100

    error page diferent language

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -1,15 +1,26 @@
 import ButtonPrimary from "../components/General/ButtonPrimary"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 
 const Error = () => {
+  const { t, i18n } = useTranslation("common");
+  const error = t("Error", { returnObjects: true });
   return (
     <div className='text-center justify-center items-center flex flex-col gap-y-10 p-20'>
     <img src='/notfound.png' alt='Not Found' width='200' height='200'/>
-    <p className='text-3xl'>Oops. This page has gone missing.</p>
-    <p className='text-xl'>You may have mistyped the address or the page may have moved.</p>
-    <ButtonPrimary text={'Go Home'} locate={'/'}/>
+    <p className='text-3xl'>{error.header}.</p>
+    <p className='text-xl'>{error.desc}</p>
+    <ButtonPrimary text={error.button} locate={'/'}/>
   </div>
   )
+}
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
 }
 
 export default Error
