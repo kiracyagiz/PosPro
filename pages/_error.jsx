a5e3ca23b79051ff68f497a03e6d1b20df18bdcf
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-12-29 11:02:59 +0100

    mnd

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -1,8 +1,14 @@
+import ButtonPrimary from "../components/General/ButtonPrimary"
 
 
 const Error = () => {
   return (
-    <div>Custom Error Page</div>
+    <div className='text-center justify-center items-center flex flex-col gap-y-10 p-20'>
+    <img src='/notfound.png' alt='Not Found' width='200' height='200'/>
+    <p className='text-3xl'>Oops. This page has gone missing.</p>
+    <p className='text-xl'>You may have mistyped the address or the page may have moved.</p>
+    <ButtonPrimary text={'Go Home'} locate={'/'}/>
+  </div>
   )
 }
 
